4933a97b7f22359fde94f482ea7c9ff0f26c8f90
anshyadav <anshyadav4244@gmail.com>
2023-04-23 19:49:53 +0530

    finalised project

---
+++ Mac/Capture_Image.py
@@ -0,0 +1,102 @@
+import csv
+
+import cv2
+import os
+
+import os.path
+
+import validate_email
+import re
+
+# counting the numbers
+
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+
+    return False
+
+
+
+def ismail(email):
+
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    return bool(re.match(pattern, email))
+
+# Take image function
+
+def takeImages():
+
+
+    Id = input("Enter Your Id: ")
+    name = input("Enter Your Name: ")
+    email = input("Enter Your email: ")
+
+    print(ismail(email));
+
+    if(is_number(Id) and name.isalpha() and ismail(email) ): #
+        cam = cv2.VideoCapture(0)
+        harcascadePath = "haarcascade_frontalface_default.xml"
+        detector = cv2.CascadeClassifier(harcascadePath)
+        sampleNum = 0
+
+        while(True):
+            ret, img = cam.read()
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = detector.detectMultiScale(gray, 1.3, 5, minSize=(30,30),flags = cv2.CASCADE_SCALE_IMAGE)
+            for(x,y,w,h) in faces:
+                cv2.rectangle(img, (x, y), (x+w, y+h), (10, 159, 255), 2)
+                #incrementing sample number
+                sampleNum = sampleNum+1
+                #saving the captured face in the dataset folder TrainingImage
+                cv2.imwrite("TrainingImage" + os.sep +name + "."+Id + '.' +
+                            str(sampleNum) + ".jpg", gray[y:y+h, x:x+w])
+                #display the frame
+                cv2.imshow('frame', img)
+
+            #wait for 100 miliseconds
+            if cv2.waitKey(50) & 0xFF == ord('q'):
+                break
+            # break if the sample number is more than 100
+            elif sampleNum > 100:
+                break
+
+        cam.release()
+        cv2.destroyAllWindows()
+        for i in range(1, 5):
+            cv2.waitKey(1)
+
+        res = "Images Saved for ID : " + Id + " Name : " + name  +  " Email : " + email 
+        header=["Id", "Name" , "Email"]       
+        row = [Id, name , email]      
+        if(os.path.isfile("StudentDetails"+os.sep+"StudentDetails.csv")):
+            with open("StudentDetails"+os.sep+"StudentDetails.csv", 'a+') as csvFile:
+                writer = csv.writer(csvFile)
+                writer.writerow(j for j in row)
+            csvFile.close()
+        else:
+            with open("StudentDetails"+os.sep+"StudentDetails.csv", 'a+') as csvFile:
+                writer = csv.writer(csvFile)
+                writer.writerow(i for i in header)
+                writer.writerow(j for j in row)
+            csvFile.close()
+    else:
+        if(is_number(Id)):
+            print("Enter Alphabetical Name")
+        if(name.isalpha()):
+            print("Enter Numeric ID")
+        if(ismail(email)):
+            print("Enter correct Mail")
+
+
